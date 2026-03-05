4f9f4e32b4a3169b8730f8d44633b940c76b8a50
drnaylor <93977310+drnaylor@users.noreply.github.com>
2026-03-05 14:04:40 +0000

    Create history tables for additional dashboard

--- petrol_prices_etl_pipeline/transformations/silver_to_gold.sql
+++ petrol_prices_etl_pipeline/transformations/silver_to_gold.sql
@@ -34,8 +34,54 @@ SELECT DISTINCT
     postcode,
     reported_latitude,
     reported_longitude
-FROM silver.petrol_prices.forecourts
-WHERE __END_AT IS NULL AND temporary_closure = FALSE AND permanent_closure = FALSE;
+FROM silver.petrol_prices.forecourts;
+
+-- -- FORECOURT HISTORY
+
+CREATE MATERIALIZED VIEW gold.petrol_prices.forecourt_history AS
+WITH initial AS (
+    SELECT DISTINCT
+        entry_timestamp,
+        DATE_FORMAT(entry_timestamp, 'yyyyMMdd') AS date_key,
+        forecourt_id,
+        trading_name,
+        brand_name,
+        temporary_closure,
+        permanent_closure,
+        CASE WHEN motorway_service_station_flag THEN 'Motorway' WHEN supermarket_flag THEN 'Supermarket' ELSE 'Other' END AS forecourt_type,
+        postcode,
+        reported_latitude,
+        reported_longitude
+    FROM silver.petrol_prices.forecourts
+),
+ordered AS (
+    SELECT 
+        date_key,
+        forecourt_id,
+        trading_name,
+        brand_name,
+        temporary_closure,
+        permanent_closure,
+        forecourt_type,
+        postcode,
+        reported_latitude,
+        reported_longitude,
+        ROW_NUMBER() OVER (PARTITION BY date_key, forecourt_id ORDER BY entry_timestamp DESC) AS rn -- ensures the last entry is given a row number of 1
+    FROM initial
+)
+SELECT
+    date_key,
+    forecourt_id,
+    trading_name,
+    brand_name,
+    temporary_closure,
+    permanent_closure,
+    forecourt_type,
+    postcode,
+    reported_latitude,
+    reported_longitude
+FROM ordered
+WHERE rn = 1;
 
 -- -- ORIGINAL PRICES
 
@@ -54,7 +100,7 @@ SELECT
 FROM silver.petrol_prices.prices
 LEFT SEMI JOIN latest_entries l USING (price_timestamp, forecourt_id, fuel_type_code);
 
--- -- CLEANED PRICES
+-- -- CLEANED LATEST PRICES
 
 CREATE MATERIALIZED VIEW gold.petrol_prices.latest_prices AS
 WITH latest_entries AS (
@@ -70,3 +116,53 @@ SELECT
     price
 FROM silver.petrol_prices.prices
 LEFT SEMI JOIN latest_entries l USING (price_timestamp, forecourt_id, fuel_type_code);
+
+-- -- CLEANED PRICES HISTORY (by date)
+
+-- We take the price of fuel at the latest point of the day
+CREATE MATERIALIZED VIEW gold.petrol_prices.price_history AS
+WITH initial AS (
+    SELECT
+        price_timestamp AS last_update,
+        DATE_FORMAT(price_timestamp, 'yyyyMMdd') AS date_key,
+        postcode,
+        forecourt_id,
+        fuel_type_code,
+        price
+    FROM silver.petrol_prices.prices
+),
+ordered AS (
+    SELECT
+        last_update,
+        date_key,
+        postcode,
+        forecourt_id,
+        fuel_type_code,
+        price,
+        ROW_NUMBER() OVER (PARTITION BY date_key, forecourt_id, fuel_type_code ORDER BY last_update DESC) AS rn -- ensures the last entry is given a row number of 1
+    FROM initial
+)
+SELECT
+    last_update,
+    date_key,
+    postcode,
+    forecourt_id,
+    fuel_type_code,
+    price
+FROM ordered
+WHERE rn = 1 AND price IS NOT NULL;
+
+-- DATE DIMENSION
+
+CREATE MATERIALIZED VIEW gold.petrol_prices.dates AS
+WITH min_date AS (
+    SELECT DATE_TRUNC(MIN(price_timestamp), 'DAY') AS first_date FROM silver.petrol_prices.prices
+),
+dates AS (
+    SELECT EXPLODE(SEQUENCE(first_date, CURRENT_DATE(), INTERVAL 1 DAY)) AS date
+    FROM min_date
+)
+SELECT
+    DATE_FORMAT(date, 'yyyyMMdd') AS date_key,
+    date
+FROM dates;
